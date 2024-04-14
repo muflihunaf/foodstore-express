b72f9e3dbea863fac2d24c3f6e32a44517ccedbb
muflihunaf <firmanedam19@gmail.com>
2024-04-15 03:36:14 +0700

    [FEAT] cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 const productRouter = require('./app/products/router');
 const categoriesRouter = require('./app/categories/router');
@@ -26,6 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use(decodeToken());
 
 app.use('/auth', authRouter);
