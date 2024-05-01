3e7cc05005e7d03915c6892f12b2835ea2050351
muflihunaf <firmanedam19@gmail.com>
2024-05-01 14:53:14 +0700

    add FE

---
+++ public/precache-manifest.0569b320297addc1ea7c018fdcee4a13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2d82178c569f0fbddb3342603bc63b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ab5413de34cbffe4bac",
+    "url": "/static/css/2.cc4e79c1.chunk.css"
+  },
+  {
+    "revision": "aad354d1ddf1702bb72a",
+    "url": "/static/css/main.d146a238.chunk.css"
+  },
+  {
+    "revision": "9ab5413de34cbffe4bac",
+    "url": "/static/js/2.2146e78b.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.2146e78b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad354d1ddf1702bb72a",
+    "url": "/static/js/main.52548783.chunk.js"
+  },
+  {
+    "revision": "b3de2f7140cf187cde16",
+    "url": "/static/js/runtime-main.70270d15.js"
+  }
+]);
